83d9f2dcafce551d7af435a4dfcf17d55f7466da
dakeenomar-design <dakeenomar@gmail.com>
2026-08-13 15:42:09 +0000

    tutorial3 git push git commit -mtutorial3

---
+++ tutorial_3/main.java
@@ -0,0 +1,14 @@
+package tutorial_3;
+
+public class main {
+    
+    public static void main(String[] args){
+        Person p1 = new Person("Aisha", "P001");
+        Person p2 = new Lecturer("Sara", "L001");
+        Person p3 = new Student("Ahmed", "S001");
+
+        p1.introduce();
+        p2.introduce();
+        p3.introduce();
+    }
+}
