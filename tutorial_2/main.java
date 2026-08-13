436dee3fe7a2e383d42c3f731728a320347904b7
dakeenomar-design <dakeenomar@gmail.com>
2026-08-13 16:13:38 +0000

    tutorial2

--- tutorial_2/main.java
+++ tutorial_2/main.java
@@ -5,10 +5,9 @@ public class main {
     public static void main(String[] args) {
         //coding starts here;
 
-        Student s1 = new Student("Tariq",23,4.0);
+        Student s1 = new Student("omar",23,4.0);
         s1.displayInfo();
         s1.study();
         s1.takeExam();
     }
 }
-
