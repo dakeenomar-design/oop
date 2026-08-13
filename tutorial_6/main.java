604037a4063f0bd6a565a98d10b23feac48cec1a
dakeenomar-design <dakeenomar@gmail.com>
2026-08-13 15:54:57 +0000

    tutorial6

---
+++ tutorial_6/main.java
@@ -0,0 +1,11 @@
+package tutorial_6;
+
+public class main {
+    
+    public static void main(String[] args) {
+        lecturer l = new lecturer("L101", "Dr Tarig","OOP", "Faculty of Information Technology");
+        l.displayInfo();
+        l.displaySubject();
+        l.displayDepartment();
+    }
+}
